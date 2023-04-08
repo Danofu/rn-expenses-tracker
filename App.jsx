783ef3579b4032bf7baa3721650da8b2104e92ae
Daniil Borysov <danofu13@gmail.com>
2023-04-08 15:39:51 +0200

    Setup environment

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -9,6 +9,8 @@ export default function App() {
     </View>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
-module.exports = function(api) {
+module.exports = (api) => {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        { extensions: ['.android.js', '.android.jsx', '.ios.js', '.ios.jsx', '.js', '.jsx'], root: ['assets', 'src'] },
+      ],
+    ],
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
   };
 };
