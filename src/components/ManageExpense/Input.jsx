99c53c619aa56de6e43539fc66def98ec2162104
Daniil Borysov <danofu13@gmail.com>
2023-04-12 17:00:06 +0200

    Style Input component

--- src/components/ManageExpense/Input.jsx
+++ src/components/ManageExpense/Input.jsx
@@ -1,11 +1,19 @@
-import { Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import PropTypes from 'prop-types';
 
+import { GlobalStyles } from 'constants/styles';
+
 function Input({ label, textInputConfig }) {
+  const inputStyles = [styles.input];
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
 }
@@ -15,3 +23,26 @@ Input.propTypes = { label: PropTypes.string, textInputConfig: PropTypes.object }
 Input.defaultProps = { label: '', textInputConfig: {} };
 
 export default Input;
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    borderRadius: 6,
+    color: GlobalStyles.colors.primary700,
+    fontSize: 18,
+    padding: 6,
+  },
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: 'top',
+  },
+  label: {
+    color: GlobalStyles.colors.primary100,
+    fontSize: 12,
+    marginBottom: 4,
+  },
+});
